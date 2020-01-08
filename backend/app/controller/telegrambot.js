be338c811e2bacb3eb37a9d0a93734274c7a1977
韦海晖 <franciswei98@gmail.com>
2020-01-08 15:50:54 +0800

    fix: return code to avoid error

--- backend/app/controller/telegrambot.js
+++ backend/app/controller/telegrambot.js
@@ -10,7 +10,7 @@ class telegramController extends Controller {
     const { ctx } = this;
     const { chat_id, text } = ctx.request.body;
     const result = await this.service.telegram._sendMessage({ chat_id, text });
-    ctx.body = result;
+    ctx.body = { ...ctx.msg.success, result };
   }
 
   /**
@@ -20,7 +20,7 @@ class telegramController extends Controller {
     const { ctx } = this;
     const { chat_id, html } = ctx.request.body;
     const result = await this.service.telegram._sendMessage({ chat_id, text: html, parse_mode: 'HTML' });
-    ctx.body = result;
+    ctx.body = { ...ctx.msg.success, result };
   }
 
   /**
@@ -31,7 +31,7 @@ class telegramController extends Controller {
     const { chat_ids, text } = ctx.request.body;
     const requests = chat_ids.map(chat_id => this.service.telegram._sendMessage({ chat_id, text }));
     const result = await Promise.all(requests);
-    ctx.body = result;
+    ctx.body = { ...ctx.msg.success, result };
   }
 
   /**
@@ -42,13 +42,13 @@ class telegramController extends Controller {
     const { chat_ids, html } = ctx.request.body;
     const requests = chat_ids.map(chat_id => this.service.telegram._sendMessage({ chat_id, text: html, parse_mode: 'HTML' }));
     const result = await Promise.all(requests);
-    ctx.body = result;
+    ctx.body = { ...ctx.msg.success, result };
   }
 
   async getAllTelegramBindedUser() {
     try {
       const result = await this.service.telegram.getAllTelegramBindedUser();
-      this.ctx.body = { result };
+      this.ctx.body = { ...this.ctx.msg.success, result };
     } catch (error) {
       this.ctx.body = { error };
     }
