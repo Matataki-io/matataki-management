41f960c43214c7fafe9717ebcb48eb77c98f71e6
xiaotiandada <istianlei@qq.com>
2021-02-26 15:48:09 +0800

    feat: 申请队列接口

---
+++ backend/app/schedule/token_application.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Subscription = require('egg').Subscription;
+
+// 任务领取奖励 定时执行转账
+class TokenApplication extends Subscription {
+  // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  static get schedule() {
+    return {
+      interval: '3m',
+      type: 'worker',
+    };
+  }
+
+  // subscribe 是真正定时任务执行时被运行的函数
+  async subscribe() {
+    this.ctx.service.minetokenApplication.agreeCreate();
+  }
+}
+
+module.exports = TokenApplication;
